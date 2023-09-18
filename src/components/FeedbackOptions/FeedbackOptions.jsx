58ac3bb75e65297942ca51383fd3100197ca906d
Roman Kryhul <104628639+UmyaFamilia@users.noreply.github.com>
2023-09-19 01:35:33 +0300

    ....

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,13 +1,13 @@
 import { nanoid } from 'nanoid';
-
+import css from './FeedbackOptions.module.css';
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
-    <div>
-      <ul>
+    <div className={css.coverList}>
+      <ul className={css.cover}>
         {options.map(a => {
           return (
             <li key={nanoid()}>
-              <button name={a} onClick={onLeaveFeedback}>
+              <button name={a} onClick={onLeaveFeedback} className={css.button}>
                 {a}
               </button>
             </li>
